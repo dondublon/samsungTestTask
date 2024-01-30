20af7159069b8f6dbb9716237441bf3a665ff147
Yaroslav Benin <dondublon1@yandex.ru>
2024-01-30 14:05:06 +0400

    COmmong methods added.

--- src/queue_.py
+++ src/queue_.py
@@ -8,3 +8,13 @@ class TaskQueue:
     def get_task(self, available_resources: Resources) -> Task | None:
         """If there is no such task, we return None."""
         pass
+
+    # region Additional methods for testing:
+    def clear(self):
+        """Clears all the queue, be careful!"""
+        pass
+
+    def get_all(self, available_resources: Resources) -> list[Task]:
+        """Just for control, not for production using."""
+        pass
+    # endregion
